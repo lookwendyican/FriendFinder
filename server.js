a9b1c287b63e1fd8f9edd7a152186b568754892d
lookwendyican <49107563+lookwendyican@users.noreply.github.com>
2019-09-09 13:14:36 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ var express = bodyParser("body-parser");
 var path = require("path");
 
 var app = express():
-var port = process.env.PORT || 3000:
+var port = process.env.PORT || 3000;
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }());
+app.use(bodyParser.text());            
+app.use(bodyParser.json({ type: "application/vnd.api+json" }());
+
+app.use(express.static("app/public"));
+
 
 app.listen(port, () => console.log(Listening on port %s", port));
