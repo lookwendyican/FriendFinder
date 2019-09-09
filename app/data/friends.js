5afdd6862591dff792cc77841b32f49e07958e17
lookwendyican <49107563+lookwendyican@users.noreply.github.com>
2019-09-09 13:29:29 -0400

    Update friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,1 +1,64 @@
-initial setup
+var friends + [
+  {
+    name: "Yon Gonzalez",
+    photo: "",
+    scores: [5,3,2,5,4,1,2,5,3,4]
+  }
+  
+   {
+    name: "Mario Casas",
+    photo: "",
+    scores: [4,5,1,3,5,2,4,5,3,4]
+  }
+  
+   {
+    name: "Carla Morrison",
+    photo: "",
+    scores: [3,1,5,4,2,3,4,1,5,2]
+  }
+
+ {
+    name: "Henry Cavill",
+    photo: "",
+    scores: [5,3,2,4,5,4,2,5,3,4]
+  }
+
+ {
+    name: "Clara Lago",
+    photo: "",
+    scores: [4,3,5,3,4,1,2,5,3,4]
+  }
+ 
+   {
+    name: "Aitor Luna",
+    photo: "",
+    scores: [5,5,5,4,2,5,2,5,3,4]
+  }
+  
+   {
+    name: "Nadia Santiago",
+    photo: "",
+    scores: [4,5,1,3,5,2,3,2,1,5]
+  }
+  
+   {
+    name: "Blanca Suarez",
+    photo: "",
+    scores: [3,5,3,1,4,3,4,1,5,2]
+  }
+
+ {
+    name: "Kit Harrington",
+    photo: "",
+    scores: [5,3,2,4,5,2,4,5,1,1]
+  }
+
+ {
+    name: "Daniel Lundh",
+    photo: "",
+    scores: [5,4,3,2,1,1,2,5,3,4]
+  }
+];
+
+               
+module.exports = friends:
