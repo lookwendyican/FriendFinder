c852e4d647b7d1bf0664c0a0580be4c22316606c
lookwendyican <49107563+lookwendyican@users.noreply.github.com>
2019-09-09 12:26:08 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-Console.log("this is the server file");
+var express = require("express");
+var express = bodyParser("body-parser");
+var path = require("path");
+
+var app = express():
+var port = process.env.PORT || 3000:
+
+app.listen(port, () => console.log(Listening on port %s", port));
