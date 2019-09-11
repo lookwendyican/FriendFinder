304b236440d6c3356fe2652c6fc6290254b054c4
lookwendyican <wendyalers@gmail.com>
2019-09-10 22:48:11 -0400

    added chalk npm

--- server.js
+++ server.js
@@ -2,17 +2,18 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 var nodemon = require("nodemon");
+var chalk = require("chalk");
 
-var app = express():
+var app = express();
 var port = process.env.PORT || 3000;
 
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());            
-app.use(bodyParser.json({ type: "application/vnd.api+json" }());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 app.use(express.static("app/public"));
 
 
-app.listen(port, () => console.log(Listening on port %s", port));
+app.listen(port, () => console.log(chalk.green("Listening on port:  ", port)));
