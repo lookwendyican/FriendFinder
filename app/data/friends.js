e7b936e9877a47bdde9c80d3573fa5eafa11a198
lookwendyican <wendyalers@gmail.com>
2019-09-15 11:59:13 -0400

    added commas between objects in friends js file

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,57 +1,57 @@
-var friends + [
+var friends = [
   {
     name: "Yon Gonzalez",
     photo: "https://images.app.goo.gl/KDpPKL8vMULPsxoM8",
     scores: [5,3,2,5,4,1,2,5,3,4]
-  }
+  },
   
    {
     name: "Mario Casas",
     photo: "https://images.app.goo.gl/rLhkAa176EkZBtNT9",
     scores: [4,5,1,3,5,2,4,5,3,4]
-  }
+  },
   
    {
     name: "Carla Morrison",
     photo: "https://images.app.goo.gl/AzuQZyQFsZonk2yS7",
     scores: [3,1,5,4,2,3,4,1,5,2]
-  }
+  },
 
  {
     name: "Macarena Garcia",
     photo: "https://images.app.goo.gl/ecHhazdL1NZPK1kNA",
     scores: [5,3,2,4,5,4,2,5,3,4]
-  }
+  },
 
  {
     name: "Clara Lago",
     photo: "https://images.app.goo.gl/sqho9EGMnrKcD9rZ8",
     scores: [4,5,5,3,4,3,2,5,5,4]
-  }
+  },
  
    {
     name: "Aitor Luna",
     photo: "https://images.app.goo.gl/GtAwPmAtYw49TGBW8",
     scores: [5,2,5,4,2,2,2,5,2,4]
-  }
+  },
   
    {
     name: "Nadia Santiago",
     photo: "https://images.app.goo.gl/zLcHkAJTb2thZCy19",
     scores: [3,2,1,3,2,2,3,2,1,2]
-  }
+  },
   
    {
     name: "Blanca Suarez",
     photo: "https://images.app.goo.gl/rYMQowLf71Mrc9Ri7",
     scores: [3,5,3,1,4,3,4,1,5,2]
-  }
+  },
 
  {
     name: "Alex Garcia",
     photo: "https://images.app.goo.gl/v26DoZG2eDFk5PGr8",
     scores: [5,1,4,1,5,1,4,1,5,1]
-  }
+  },
 
  {
     name: "Daniel Lundh",
