8f250897746ab8209e95fad72b4af3f9960479e7
lookwendyican <wendyalers@gmail.com>
2019-09-18 21:50:38 -0400

    added submit functionality  to app.js

--- app/public/app.js
+++ app/public/app.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
     //Capture the form input
     $('#submit').on('click', function(event) {
         event.preventDefault();
+        console.log("submitted");
         // Create an object for the user's data
         var userData = {
             name: $("#userName").val(),
@@ -26,13 +27,13 @@ $(document).ready(function () {
     }); //End of submmit function
 
     //AJAX to post data to the friends API
-    $.post('/api/friends', userData, function(data) {
-        console.log(data);
-         // Grab the result from the AJAX post so that the best match's name and photo are displayed.
-         $("#match-name").text(data.name);
-         $("#match-img").attr("src", data.photo);
-         // Show the modal with the best match
-         $("#results-modal").modal("toggle");
-     });
+    // $.post('/api/friends', userData, function(data) {
+    //     console.log(data);
+    //      // Grab the result from the AJAX post so that the best match's name and photo are displayed.
+    //      $("#match-name").text(data.name);
+    //      $("#match-img").attr("src", data.photo);
+    //      // Show the modal with the best match
+    //      $("#results-modal").modal("toggle");
+    //  });
 
 });//End of document ready
